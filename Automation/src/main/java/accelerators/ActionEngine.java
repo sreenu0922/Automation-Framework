c963dbd979463d03036b5299208b6d3270c50c2d
srinivas n <srinivasn@nbos.vinisoft.com>
2020-04-17 12:48:47 +0530

    Added swipe method in ActionEngine class

--- Automation/src/main/java/accelerators/ActionEngine.java
+++ Automation/src/main/java/accelerators/ActionEngine.java
@@ -1,12 +1,13 @@
 package accelerators;
 
-import java.awt.Dimension;
+
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 
 import javax.imageio.ImageIO;
 
@@ -14,9 +15,14 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.Dimension;
 
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import rough.SecondClass.DIRECTION;
 import support.Reporter;
-import support.Logger;
 
 public class ActionEngine extends TestEngine{
 	
@@ -110,4 +116,98 @@ public static void fullScreenShot(String fileName) throws Exception {
 	   ImageIO.write(image, "jpeg", new File(fileName));
 	 
 	}
+
+
+
+
+/*
+ * Swipe up 
+	Swipe down 
+	Swipe left 
+	Swipe Right 
+ * 
+ * using swipe method we can perform above operations
+ * eg: swipe(driver,DIRECTION.UP);
+ * 
+ * swipe(driver,DIRECTION.RIGHT);
+ * 
+ */
+
+public enum DIRECTION {
+    DOWN, UP, LEFT, RIGHT;
+}
+
+
+public static void swipe(MobileDriver driver, DIRECTION direction) {
+   
+	
+	Dimension size = driver.manage().window().getSize();
+
+    int startX = 0;
+    int endX = 0;
+    int startY = 0;
+    int endY = 0;
+
+    switch (direction) {
+        case RIGHT:
+            startY = (int) (size.height / 2);
+            startX = (int) (size.width * 0.90);
+            endX = (int) (size.width * 0.05);
+            new TouchAction(driver)
+          .press(PointOption.point(startX, startY))
+          .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(2L)))
+          .moveTo(PointOption.point(endX, startY)).release().perform();
+             break;
+
+        case LEFT:
+            startY = (int) (size.height / 2);
+            startX = (int) (size.width * 0.05);
+            endX = (int) (size.width * 0.90);
+            new TouchAction(driver)
+            .press(PointOption.point( startX, startY ))
+            .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(2L)))
+            .moveTo(PointOption.point(endX, startY))
+            .release()
+            .perform();
+
+            break;
+
+        case UP:
+        	
+        	startX = (size.width / 2);
+            endY = (int) (size.height * 0.70);
+            startY = (int) (size.height * 0.30);
+           
+            new TouchAction(driver)
+            .press(PointOption.point(startX, startY))
+            .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(2L)))
+            .moveTo(PointOption.point(startX, endY))
+            .release()
+            .perform();
+                  
+            break;
+
+
+        case DOWN:
+        	
+        	startX = (size.width / 2);
+            startY = (int) (size.height * 0.70);
+            endY = (int) (size.height * 0.30);
+                   
+            new TouchAction(driver)
+            .press(PointOption.point(startX, startY))
+            .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(2L)))
+            .moveTo(PointOption.point(startX, endY))
+            .release()
+            .perform();
+                 
+
+            break;
+
+    }
+    
+    
+    
+}
+
 }
