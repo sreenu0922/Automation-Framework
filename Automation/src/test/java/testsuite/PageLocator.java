00d7469d03ad00969a9448d969114cf0083cb1d0
srinivas n <srinivasn@nbos.vinisoft.com>
2020-04-20 12:08:19 +0530

    Added elements for Refine,next, location and radious dropdown in caare center list page

--- Automation/src/test/java/testsuite/PageLocator.java
+++ Automation/src/test/java/testsuite/PageLocator.java
@@ -22,10 +22,24 @@ public class PageLocator extends ActionEngine {
 	 * 
 	 */
 	public static By lsChoseCareCenter = By.id("com.care.wps.debug:id/choice_one");
-	public static By txRecommed= By.xpath("//*[@text='Recommended' ] ");
+	public static By txRecommed = By.xpath("//*[@text='Recommended' ] ");
 	
 	//care center list
-	public static By lstCareCenter= By.xpath("//*[@resource-id='com.care.wps.debug:id/care_center_item_layout']");
+	public static By lstCareCenter = By.xpath("//*[@resource-id='com.care.wps.debug:id/care_center_item_layout']");
+	public static By btnRefine = By.id("com.care.wps.debug:id/refine_cta");
+	public static By btnNext = By.id("com.care.wps.debug:id/next_cta");
+	//After click on next button app was displaying dilogbox below are elemets for dilogbox
+	public static By dlgText = By.id("com.care.wps.debug:id/dialog_description");
+	public static By dlgButtonOk = By.id("com.care.wps.debug:id/dialog_positive_button");
+	
+	//After click on Refine button app was displaying change location and radious below are elemtes related to change location and radious
+	public static By frmLocation = By.id("com.care.wps.debug:id/location");
+	public static By frmSearchRadius = By.id("com.care.wps.debug:id/search_radius");
+	public static By milespDropdown = By.xpath("//* [@class='android.widget.TextView' ] ");
+	public static By btnMilesDone = By.id("com.care.wps.debug:id/done");
+	public static By BtnCancel = By.id("com.care.wps.debug:id/cancel");
+	public static By BtnDone = By.id("com.care.wps.debug:id/done");
+	
 	
 	
 
