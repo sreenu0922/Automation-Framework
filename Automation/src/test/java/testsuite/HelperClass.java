b076c26ee629ed78a2f2572db0236abf31936c33
Sreeranga <ranga3229@gmail.com>
2020-04-20 20:42:04 +0530

    Added page locatorsfor backup days and Booknow

--- Automation/src/test/java/testsuite/HelperClass.java
+++ Automation/src/test/java/testsuite/HelperClass.java
@@ -1,7 +1,20 @@
 package testsuite;
 
-import org.openqa.selenium.By;
-
 public class HelperClass extends PageLocator {
+
+	
+	//Author Sreeranga
+	// Navigate to use my backup days
+	public void navigatetousemybackupdays() throws Throwable {
+		click(BackUpDays,"Backupdays");
+
+	}
+	
+	// NAvigate to Book Now Button
+	public void navigatetoBookNow() throws Throwable {
+
+		click(BookNow , "BookNowButton");
+
+	}
 
 }
--- Automation/src/test/java/testsuite/PageLocator.java
+++ Automation/src/test/java/testsuite/PageLocator.java
@@ -4,6 +4,14 @@ import org.openqa.selenium.By;
 import accelerators.ActionEngine;
 
 public class PageLocator extends ActionEngine {
+	
+	//author Sreeranga
+	
+	//Select Use my backUp days
+	public static By  BackUpDays= By.id("use_my_backup_days");
+	//select BookNow Button
+	public static By BookNow = By.id("ctabutton");
+	
+	}
 
 
-}
