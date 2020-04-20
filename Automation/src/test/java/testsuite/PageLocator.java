f9e999bd3bd7db7f2768ba608858af2dfe21ffb9
srinivas n <srinivasn@nbos.vinisoft.com>
2020-04-20 11:16:31 +0530

    Added elements for care center selection in PageLocators page

--- Automation/src/test/java/testsuite/PageLocator.java
+++ Automation/src/test/java/testsuite/PageLocator.java
@@ -7,5 +7,26 @@ import accelerators.ActionEngine;
 public class PageLocator extends ActionEngine {
 	
 	public static By menu = By.xpath("//a[@aria-label='navigation menu']");
+	
+	
+	
+	
+	
+	
+	/*@author srinivas n 20th April 2020
+	 * adding elements for incenter care selection
+	 * 
+	 * verify the recommended care center
+	 * Select 1st care center
+	 * Select 2nd care center
+	 * 
+	 */
+	public static By lsChoseCareCenter = By.id("com.care.wps.debug:id/choice_one");
+	public static By txRecommed= By.xpath("//*[@text='Recommended' ] ");
+	
+	//care center list
+	public static By lstCareCenter= By.xpath("//*[@resource-id='com.care.wps.debug:id/care_center_item_layout']");
+	
+	
 
 }
