7d683f1cc6964a801ce45942c9263dd44a5adfbb
PravallikaNakka <63784551+PravallikaNakka@users.noreply.github.com>
2020-04-20 17:05:47 +0530

    Update PageLocator.java

--- PageLocator.java
+++ PageLocator.java
@@ -9,13 +9,13 @@ public class PageLocator extends ActionEngine {
 
 	//Pravallika
 	// Locators for Select care options page
-	public static By CoronaAlert = By.xpath("//*[contains(text(),'Worried about coronavirus? Here's what you need to know >')]");
-	public static By NeedforCarepage = By.xpath("//*[contains(text(),'Who do you need care for?')]");
-	public static By OptionSelectMykids = By.xpath("//*[contains(text(),'My kids')]");
-	public static By OptionSelectMyAdultLovedOnes = By.xpath("//*[contains(text(),'My adult loved ones')]");
-	public static By OptionSelectMyPets = By.xpath("//*[contains(text(),'My pets')]");
-	public static By OptionSelectMyhome = By.xpath("//*[contains(text(),'My home')]");
-	public static By learnMoreAboutCare = By.xpath("//*[contains(text(),'Learn about my Care@Work benefits')]");
+	public static By CoronaAlert = By.xpath("//*[contains(@text,'Worried about coronavirus? Here's what you need to know >')]");
+	public static By NeedforCarepage = By.xpath("//*[contains(@text,'Who do you need care for?')]");
+	public static By OptionSelectMykids = By.xpath("//*[contains(@text,'My kids')]");
+	public static By OptionSelectMyAdultLovedOnes = By.xpath("//*[contains(@text,'My adult loved ones')]");
+	public static By OptionSelectMyPets = By.xpath("//*[contains(@text,'My pets')]");
+	public static By OptionSelectMyhome = By.xpath("//*[contains(@text,'My home')]");
+	public static By learnMoreAboutCare = By.xpath("//*[contains(@text,'Learn about my Care@Work benefits')]");
 	public static By Settings = By.xpath("//*[contains(@id,'profile_settings')]");
 	public static By PageHome_Title= By.xpath("//*[contains(@resource-id,'home_bg')]");
 }
