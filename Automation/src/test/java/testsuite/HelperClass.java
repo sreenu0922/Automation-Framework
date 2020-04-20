31e6c6abf913aab0cf5edb99dc6cd6b475b246fd
bhavya.narla <bhavyan@nbos.vinisoft.com>
2020-04-20 20:08:57 +0530

    added page locators

--- Automation/src/test/java/testsuite/HelperClass.java
+++ Automation/src/test/java/testsuite/HelperClass.java
@@ -3,5 +3,21 @@ package testsuite;
 import org.openqa.selenium.By;
 
 public class HelperClass extends PageLocator {
-
+	
+	
+//author by Bhavya
+	public void navigaateToUat() throws Throwable {
+		click(settingsicon ,"settingicon");
+		click(uat25 , "uat25");
+	}
+	public void navigateToEnroll() throws Throwable {
+		
+	click(enroll ,"enrollbutton");
+	}
+	
+	public void selectCompany() throws Throwable {
+	click(companysearch , "company_search");
+	
+	}
+	
 }
--- Automation/src/test/java/testsuite/PageLocator.java
+++ Automation/src/test/java/testsuite/PageLocator.java
@@ -4,6 +4,43 @@ import org.openqa.selenium.By;
 import accelerators.ActionEngine;
 
 public class PageLocator extends ActionEngine {
+	
+	//@author by Bhavya
+	
+		//Verify Settings icon
+		//Verify Select uat25
+		//verify click on enroll
+		//verify acme from where do you work dropdown list
+		
+		public static By settingsicon = By.xpath("//*[contains(@resource-id,'settings')]");
+		public static By uat25 = By.xpath("//*[contains(@text,'uat25')]");
+		public static By enroll = By.xpath("//*[contains(@resource-id,'sign_up')]");
+		
+		//sendkeys method by giving acme
+		public static By companysearch = By.xpath("//*[contains(@resource-id,'company_search')]");
+		
+		public static By selectacme = By.id("container");
+		
+		//verify login button
+		public static By Login = By.id("sign_in");
+		
+		
+		//again select acme
+        public static By companysearch1 = By.xpath("//*[contains(@resource-id,'company_search')]");
+		
+		public static By selectacme1 = By.id("container");
+		
+		//verify email
+		public static By emailfield= By.id("email");
+		
+		//verify password
+		public static By passwordfied=By.id("passwd");
+		
+		//click on login
+		
+		public static By login = By.xpath("//*[contains(@text,'LOGIN')]");
+		
+		
 
 
 }
